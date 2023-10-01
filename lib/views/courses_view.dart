cd97263a475e0d2f304a8058730c2d9dab8c9f63
pluciuspua <e0957837@u.nus.edu>
2023-10-01 21:45:02 +0800

    Updated Course view pages

--- lib/views/courses_view.dart
+++ lib/views/courses_view.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:psa_codesprint/widgets/CourseTile.dart';
 
 class CoursesView extends StatelessWidget {
   const CoursesView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Courses View"),);
+    return GridView.count(
+      crossAxisCount: 4,
+      childAspectRatio: 2,
+      children: const [
+        CourseTile(coursename: "Intro to Python", imagepath: "assets/pythonpng.jpg"),
+        CourseTile(coursename: "Basics of ML", imagepath: "assets/MLpng.png"),
+        CourseTile(coursename: "Data Visualisation with MatPlotLib", imagepath: "assets/matplotlibpng.png"),
+        CourseTile(coursename: "Leadership & Communication", imagepath: "assets/leadershippng.png"),
+        CourseTile(coursename: "Port Operation and Management", imagepath: "assets/portpng.png"),
+        CourseTile(coursename: "Data Visualisation with R", imagepath: "assets/Rpng.jpg"),
+        CourseTile(coursename: "Workplace Safety", imagepath: "assets/safetypng.jpg"),
+        CourseTile(coursename: "Warehouse and Logistics Management", imagepath: "assets/warehouse png.png"),
+        CourseTile(coursename: "Programming with Java", imagepath: "assets/javapng.png"),
+
+      ],
+    );
   }
 }
---
+++ lib/widgets/CourseTile.dart
@@ -0,0 +1,30 @@
+import "package:flutter/material.dart";
+class CourseTile extends StatelessWidget {
+  final String coursename;
+  final String imagepath;
+  const CourseTile({Key? key,
+    required this.coursename,
+    required this.imagepath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: 75,
+        child: ElevatedButton(
+            onPressed: () {},
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10, top:20),
+                  child: SizedBox(width: 65, height: 65, child: Container(decoration: BoxDecoration(border: Border.all(width: 1.5)),child: Image.asset(imagepath,fit: BoxFit.fill ))),
+                ),
+                Text(coursename,style:const TextStyle(fontSize: 16) )
+              ],
+          )
+        ),
+      ),
+    );
+  }
+}
